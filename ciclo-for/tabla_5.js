1c10529a9681b650b70585eee1cacc1c760b39ed
Pulquedelfuerte <terron.kevin2200@gmail.com>
2024-07-11 23:46:29 -0600

    Ciclo-FOR

---
+++ ciclo-for/tabla_5.js
@@ -0,0 +1,5 @@
+console.log ("muestra la tabla del 5");
+for (let i=1; i<=10; i++);{
+    console.log ("5 * " + i + " = " + (5 * i) ); 
+}
+
---
+++ ciclo-for/tablas-1-10.js
@@ -0,0 +1,9 @@
+for (let i = 1; i <= 10; i++){
+    console.log("tabla de multiplicar");
+    for (let x= 1; x<= 10; x++){
+        console.log(i+"*"+x+"="+i*x);
+    }
+    console.log("");
+
+    }
+    
